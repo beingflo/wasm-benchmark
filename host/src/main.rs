b75a3997b74253b62725b4ef8f60ec990a2671b4
Florian Marending <florian@marending.dev>
2024-01-02 15:43:48 +0100

    Continue implementation

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,3 +1,6 @@
+use std::time::Instant;
+
+use extism::{Manifest, Plugin, Wasm};
 use rand::distributions::Uniform;
 use rand::prelude::*;
 use stats::Measurements;
@@ -5,14 +8,62 @@ use stats::Measurements;
 mod stats;
 
 fn main() {
-    println!("Hello, world!");
+    run_host();
+    run_plugin();
 }
 
-fn run_host(measurements: &mut Measurements) {
-    let mut rng = rand::thread_rng();
-    let range = Uniform::new(0, 20);
+fn run_plugin() {
+    let file = Wasm::file("./plugin.wasm");
+    let manifest = Manifest::new([file]);
+    let mut plugin = Plugin::new(&manifest, [], true).unwrap();
 
-    let vals: Vec<u64> = (0..100).map(|_| rng.sample(&range)).collect();
+    let mut measurements = Measurements::new();
+
+    for _ in 0..100 {
+        let mut rng = rand::thread_rng();
+        let range = Uniform::new(-100, 100);
+
+        let start = Instant::now();
+
+        let vals: Vec<i64> = (0..10000).map(|_| rng.sample(&range)).collect();
+
+        let extism::convert::Json(result) = plugin
+            .call::<extism::convert::Json<Vec<i64>>, extism::convert::Json<Vec<i64>>>(
+                "run",
+                extism::convert::Json(vals),
+            )
+            .unwrap();
+
+        let end = Instant::now();
+
+        measurements.insert((end - start).as_micros());
+
+        assert!(result.len() > 10);
+    }
+
+    measurements.print_results();
+}
+
+fn run_host() {
+    let mut measurements = Measurements::new();
+
+    for _ in 0..100 {
+        let mut rng = rand::thread_rng();
+        let range = Uniform::new(-100, 100);
+
+        let start = Instant::now();
+
+        let vals: Vec<i64> = (0..10000).map(|_| rng.sample(&range)).collect();
+        let result = run(vals);
+
+        let end = Instant::now();
+
+        measurements.insert((end - start).as_micros());
+
+        assert!(result.len() > 10);
+    }
+
+    measurements.print_results();
 }
 
 fn run(x: Vec<i64>) -> Vec<i64> {
